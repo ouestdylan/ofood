67df6fcf1361bdfcf9850fa44f9c19e31a77c28f
Sami Barchouchi <sami.barchouchi@gmail.com>
2022-11-09 11:33:25 +0100

    sessions ok

--- Front/app/controllers/mainController.js
+++ Front/app/controllers/mainController.js
@@ -11,8 +11,6 @@ const mainController = {
             let result = await fetch(backUrl + "recipes")
             let recipes = await result.json();
             const filterRecipes =  recipes.filter(recipe => recipe.rate > 7).slice(0, 5)
-
-            console.log(recipes);
             
             res.render('home', {recipes:filterRecipes});
 
@@ -27,7 +25,6 @@ const mainController = {
             let result = await fetch(backUrl+"recipes")
             let recipes = await result.json();
 
-            console.log(recipes);
             res.render('recipes', {recipes});
 
         } catch (error) {
@@ -36,13 +33,15 @@ const mainController = {
     },
 
     recipePage: async (req, res) => {
+
+        console.log(req.session);
+        console.log(res.locals);
         const recipeId = req.params.id
         try {
             let result = await fetch(backUrl+"recipes/"+recipeId)
             let recipe = await result.json();
 
-            console.log(recipe);
-            res.render('recipe',{recipe});
+            res.render('recipe', {recipe});
             
         } catch (error) {
             console.log(error);
--- Front/app/router.js
+++ Front/app/router.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 const mainController = require('./controllers/mainController');
 const userController = require('./controllers/userController');
+const userMiddleware = require('./middlewares/userMiddleware');
+
+router.use(userMiddleware.sessionVerification);
 
 router.get('/', mainController.homePage);
 
@@ -13,10 +16,13 @@ router.get('/recettes/:id', mainController.recipePage);
 
 router.get('/connexion', userController.loginPage);
 router.post('/connexion', userController.loginAction);
+
 router.get('/dashboard', userController.dashboardPage);
 
 router.get('/presentation', mainController.presentationPage);
 router.get('/cgu', mainController.cguPage);
 router.get('/contact', mainController.contactPage);
+
+router.get('/deconnexion', userController.disconnect);
 
 module.exports = router;
