5ddc17f7f410a7958d290aaf18419640bc1de24a
Sami Barchouchi <sami.barchouchi@gmail.com>
2022-11-14 21:52:06 +0100

    ok

--- Front/static/script/base.js
+++
@@ -1,7 +0,0 @@
-/* menu display */
-const button = document.getElementById('menu-toggler');
-function openMenu() {
-    document.getElementById('header-banner').classList.toggle('open');
-};
-button.addEventListener('click', openMenu);
-
